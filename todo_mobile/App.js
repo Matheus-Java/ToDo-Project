80378f4d9949ed8b6cb3f8e09610423031eb9a36
Leticia Cristine Duarte Nascimento <matheus.stack.20@gmail.com>
2020-05-09 01:33:01 -0300

    Criação do Filtro!

--- todo_mobile/App.js
+++ todo_mobile/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 
+// Desabilita os alertas.
+console.disableYellowBox = true;
+
 import Home from './src/views/Home';
 
 export default function App() {
--- todo_mobile/src/views/Home/styles.js
+++ todo_mobile/src/views/Home/styles.js
@@ -6,7 +6,29 @@ const styles = StyleSheet.create({
     backgroundColor: '#FFF',
     alignItems: 'center',
     justifyContent: 'flex-start'
+  },
+
+  filter: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    height: 70,
+    alignItems: 'center'
+  },
+
+  filterTextActived: {
+    fontWeight: 'bold',
+    fontSize: 18,
+    color: '#EE6B26'
+  },
+
+  filterTextInative: {
+    color: '#26295F',
+    fontSize: 18,
+    fontWeight: 'bold',
+    opacity: 0.5
   }
+
 });
 
 export default styles;
