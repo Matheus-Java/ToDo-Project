2ab42ccb4b5cbffefa0ff9b7271f16fe3d522ffd
Leticia Cristine Duarte Nascimento <matheus.stack.20@gmail.com>
2020-05-08 22:29:27 -0300

    Start dev Mobile

---
+++ todo_mobile/src/views/Home/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+import styles from './styles';
+
+// Componentes
+import Header from '../../components/Header';
+
+export default function Home() {
+  return (
+
+    <View style={styles.container}>
+      <Header showNotification={true} showBack={false} />
+    </View>
+
+  )
+}
